1fd79292465f47b9bade06aa38ab0ac066f55c87
somya kawre <somyakawre277@gmail.com>
2025-11-07 14:21:41 +0530

    Add files via upload

---
+++ even and odd.c
@@ -0,0 +1,12 @@
+ // wap you cheak number is even and mode
+ #include<stdio.h>
+ void main()
+ {
+ int no;
+ printf("\n enter any no");
+ scanf("%",&no);
+ if(no%2==0)
+ printf("\n no is even");
+ else
+ printf("\n no is odd");
+ }

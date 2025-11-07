2344ff2be7d51c11602e74758fd7303d1316c96e
somya kawre <somyakawre277@gmail.com>
2025-11-07 14:20:08 +0530

    Add files via upload

---
+++ cout.c
@@ -0,0 +1,18 @@
+//print reverce numbe
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int a,b,cout,x;
+    printf("enter value of a");
+    scanf("%d",&a);
+    b=a;
+    x=pow(10,2);
+   // while(a>0)
+    //{
+  // a=a/10;
+   // cout++;
+
+    //}
+    printf("%d",x);
+    }

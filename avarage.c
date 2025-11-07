2bcb83838f4f58af0556a6cbc5ad6bf70b4111b9
somya kawre <somyakawre277@gmail.com>
2025-11-07 14:15:38 +0530

    Add files via upload

---
+++ avarage.c
@@ -0,0 +1,11 @@
+// wap to find avarge of 3 intiger  num
+#include<stdio.h>
+void main()
+{
+int x,y,z;
+float i;
+printf("enter value of x,y&z");
+scanf("%d%d%d",&x,&y,&z);
+i=(x+y+z)/3.0;
+printf("avarage=%f",i);
+}

1d5b49f617c2322b96ae733a5c9ce8be55dea779
somya kawre <somyakawre277@gmail.com>
2025-11-07 14:24:43 +0530

    Add files via upload

---
+++ fn swap.c
@@ -0,0 +1,13 @@
+// void swap (int,int);
+#include<stdio.h>
+void main()
+{
+    int x,y,z;
+    int swap (int,int);
+    printf("enter x,&y");
+    scanf("%d%d%d",&x,&y);
+    z=x;
+    x=y;
+    y=z;
+    printf("\n swapp %d%d",x,y);
+}

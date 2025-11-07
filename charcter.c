784bbb2ca39a293a1b4c2244580c3a626e712436
somya kawre <somyakawre277@gmail.com>
2025-11-07 14:17:47 +0530

    Add files via upload

---
+++ charcter.c
@@ -0,0 +1,12 @@
+//wap to cheak any charactor is alphabate or nor ch
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("\n enter any ch");
+scanf("%c",&ch);
+if(60<=90)
+printf("charter is alphabete");
+else
+print("charter is not alphabete");
+}

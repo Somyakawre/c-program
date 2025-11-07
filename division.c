1cb74f5caa59b7514ec8eb4c38c3cb22377402e2
somya kawre <somyakawre277@gmail.com>
2025-11-07 14:20:57 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,11 @@
+//write a program to calculate the division no.
+#include <stdio.h>
+void main()
+{
+int x,y;
+float z;
+printf("enter two nos");
+ scanf("%d%d",&x,&y);
+ z=x/y;
+ printf("division=%f",z);
+ }
